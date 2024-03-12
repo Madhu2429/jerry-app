aa95b23d3da7d6876946d2f6e4f262632c464db1
Madhu2429 <mithamadhu2429@gmail.com>
2024-03-12 19:14:08 +0530

    User component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App" >
+  
+        </div>
+    );
 }
+
+
+
+function User(props) {
+    console.log(props,typeof props);
+    return (
+    <div>
+      <img className="User-profile-pic" src={props.pic} alt={props.name} />
+      <h1 className="User-name">Hello, {props.name} ❤❤</h1>;
+    </div>
+    );
+}
+
 
 export default App;
